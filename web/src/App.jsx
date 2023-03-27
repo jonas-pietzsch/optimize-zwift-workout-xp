42e5de17a5f28124376663cc96b4aae5d902eeeb
Jonas Pietzsch <jonas.pietsch93@gmail.com>
2023-03-27 16:37:17 +0200

    Web: support multiple files at once

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -4,45 +4,48 @@ import { ZwiftWorkoutParser } from "../../library/ZwiftWorkoutParser.js";
 import { download } from "./downloadFile.js";
 
 function App() {
-  const [selectedFile, setSelectedFile] = useState(undefined);
+  const [selectedFiles, setSelectedFiles] = useState(undefined);
   const optimize = async () => {
-    const workoutFileContent = await selectedFile.text();
-    const workout =
-      ZwiftWorkoutParser.parseZwiftWorkoutFile(workoutFileContent);
-    workout.optimize({
-      minimumDuration: 60,
-      intervalsDuration: 30,
-      recoveryIntervalDuration: 60,
-    });
-    const optimizedWorkoutFileContent =
-      ZwiftWorkoutParser.assembleZwiftWorkoutFile(workout);
-    download(
-      selectedFile.name.replace(".zwo", "-optimized.zwo"),
-      optimizedWorkoutFileContent
-    );
+    for (const file of selectedFiles) {
+      const workoutFileContent = await file.text();
+      const workout =
+        ZwiftWorkoutParser.parseZwiftWorkoutFile(workoutFileContent);
+      workout.optimize({
+        minimumDuration: 60,
+        intervalsDuration: 30,
+        recoveryIntervalDuration: 60,
+      });
+      const optimizedWorkoutFileContent =
+        ZwiftWorkoutParser.assembleZwiftWorkoutFile(workout);
+      download(
+        file.name.replace(".zwo", "-optimized.zwo"),
+        optimizedWorkoutFileContent
+      );
+    }
   };
 
   return (
     <div className="App">
       <h1>Optimize Zwift workouts for maximum XP</h1>
       <p>
-        Chose a .zwo file, click the button and get the optimized workout file
-        as download!
+        Chose one or multiple .zwo files, click the button and get the optimized
+        workout file(s) as download!
         <br />
-        The name of the optimized .zwo file is suffixed with ' (optimized)' so
-        you can distinguish between original and optimized.
+        Names of optimized file names are suffixed with ' (optimized)' so you
+        can distinguish between original and optimized within Zwift.
       </p>
       <div className="card">
         <div style={{ marginBottom: 20 }}>
           <input
             accept=".zwo"
+            multiple
             type="file"
-            onChange={(e) => setSelectedFile(e.target.files[0])}
+            onChange={(e) => setSelectedFiles(e.target.files)}
           />
         </div>
         <div>
-          <button disabled={!selectedFile} onClick={optimize}>
-            Optimize now
+          <button disabled={!selectedFiles} onClick={optimize}>
+            Optimize {(selectedFiles || []).length} workouts now
           </button>
         </div>
       </div>
